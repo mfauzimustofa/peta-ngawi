2dcfa2785e66ddb4e9e36a2511a51d9bec7785f5
mfauzimustofa <90197948+mfauzimustofa@users.noreply.github.com>
2026-02-15 19:17:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-/* INIT MAP */
-const map = L.map("map").setView([-7.4,111.4],11);
+const map = L.map("map");
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
 .addTo(map);
 
-/* SLIDER */
 const tahun = document.getElementById("tahun");
 const labelTahun = document.getElementById("labelTahun");
 const prev = document.getElementById("prevTahun");
@@ -14,44 +12,72 @@ const next = document.getElementById("nextTahun");
 
 labelTahun.innerHTML = tahun.value;
 
-/* UPDATE TOMBOL */
-function updateNav(){
-prev.disabled = tahun.value == tahun.min;
-next.disabled = tahun.value == tahun.max;
+let geoLayer;
+
+/* WARNA */
+function getColor(d){
+return d>50?'#084594':
+d>30?'#2171b5':
+d>20?'#4292c6':
+d>10?'#6baed6':
+d>0?'#9ecae1':'#f1f1f1';
 }
 
-/* LOAD DATA (contoh) */
+function style(feature){
+let field = "RTLH " + tahun.value;
+let value = feature.properties[field] || 0;
+
+return{
+fillColor:getColor(value),
+weight:1,
+color:"#444",
+fillOpacity:0.7
+};
+}
+
+/* LOAD DATA */
 function loadData(){
-console.log("Load data tahun:", tahun.value);
+
+if(geoLayer) map.removeLayer(geoLayer);
+
+fetch("data.geojson")
+.then(res=>res.json())
+.then(data=>{
+
+geoLayer = L.geoJSON(data,{
+style:style
+}).addTo(map);
+
+/* ZOOM TO LAYER SAAT PERTAMA */
+map.fitBounds(geoLayer.getBounds());
+
+});
+
 }
 
-/* SLIDER GESER */
+/* SLIDER */
 tahun.addEventListener("input",()=>{
 labelTahun.innerHTML = tahun.value;
-updateNav();
 loadData();
 });
 
-/* TOMBOL KIRI */
-prev.addEventListener("click",()=>{
+/* TOMBOL TAHUN */
+prev.onclick=()=>{
 if(tahun.value > tahun.min){
-tahun.value = parseInt(tahun.value) - 1;
+tahun.value--;
 labelTahun.innerHTML = tahun.value;
-updateNav();
 loadData();
 }
-});
+};
 
-/* TOMBOL KANAN */
-next.addEventListener("click",()=>{
+next.onclick=()=>{
 if(tahun.value < tahun.max){
-tahun.value = parseInt(tahun.value) + 1;
+tahun.value++;
 labelTahun.innerHTML = tahun.value;
-updateNav();
 loadData();
 }
-});
+};
 
-updateNav();
+loadData();
 
 });
