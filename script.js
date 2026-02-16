25d883b19df83e827d4ddb47e64a491efad3feaf
mfauzimustofa <90197948+mfauzimustofa@users.noreply.github.com>
2026-02-16 13:33:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-const FIELD_KEC = "KECAMATAN";
-const FIELD_DESA = "DESA";
+const FIELD_KEC="KECAMATAN";
+const FIELD_DESA="DESA";
 
-const map = L.map("map");
+const map=L.map("map");
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-const jenis = document.getElementById("jenis");
-const tahun = document.getElementById("tahun");
-const labelTahun = document.getElementById("labelTahun");
-const prev = document.getElementById("prevTahun");
-const next = document.getElementById("nextTahun");
-const kecSelect = document.getElementById("kecamatan");
-const desaSelect = document.getElementById("desa");
+const jenis=document.getElementById("jenis");
+const tahun=document.getElementById("tahun");
+const labelTahun=document.getElementById("labelTahun");
+const kecSelect=document.getElementById("kecamatan");
+const desaSelect=document.getElementById("desa");
+const prev=document.getElementById("prevTahun");
+const next=document.getElementById("nextTahun");
+const infoTotal=document.getElementById("infoTotal");
 
-labelTahun.textContent = tahun.value;
+labelTahun.textContent=tahun.value;
 
-let geoLayer, geoData;
+let geoData,geoLayer;
 
-/* AMBIL NILAI DINAMIS */
-function getValue(props){
-let prefix = jenis.value;
-let f1 = prefix+" "+tahun.value;
-let f2 = prefix+"_"+tahun.value;
-return props[f1] ?? props[f2] ?? 0;
+/* AMBIL NILAI */
+function getValue(props,prefix){
+let f1=prefix+" "+tahun.value;
+let f2=prefix+"_"+tahun.value;
+return props[f1]??props[f2]??0;
 }
 
 /* WARNA */
@@ -37,7 +37,7 @@ d>0?'#9ecae1':'#f1f1f1';
 
 function style(feature){
 return{
-fillColor:getColor(getValue(feature.properties)),
+fillColor:getColor(getValue(feature.properties,jenis.value)),
 weight:1,
 color:"#666",
 fillOpacity:0.7
@@ -45,27 +45,37 @@ fillOpacity:0.7
 }
 
 function highlight(layer){
-layer.setStyle({
-color:"red",
-weight:3,
-dashArray:"5,5"
-});
+layer.setStyle({color:"red",weight:3,dashArray:"5,5"});
 }
 
-/* LOAD GEOJSON */
+/* HITUNG TOTAL */
+function hitungTotal(features){
+
+let rtlh=0,bsps=0;
+
+features.forEach(f=>{
+rtlh+=getValue(f.properties,"RTLH");
+bsps+=getValue(f.properties,"BSPS");
+});
+
+infoTotal.innerHTML=
+`RTLH: ${rtlh.toLocaleString()} | BSPS: ${bsps.toLocaleString()}`;
+}
+
+/* LOAD DATA */
 fetch("./data-bsps-rtlh.geojson")
 .then(res=>res.json())
 .then(data=>{
 
-geoData = data;
+geoData=data;
 
-geoLayer = L.geoJSON(data,{
+geoLayer=L.geoJSON(data,{
 style:style,
-onEachFeature:(feature,layer)=>{
-layer.bindPopup(`
-<b>Desa:</b> ${feature.properties[FIELD_DESA]}<br>
-<b>Kecamatan:</b> ${feature.properties[FIELD_KEC]}<br>
-<b>Jumlah:</b> ${getValue(feature.properties)}
+onEachFeature:(f,l)=>{
+l.bindPopup(`
+<b>Desa:</b> ${f.properties[FIELD_DESA]}<br>
+<b>Kecamatan:</b> ${f.properties[FIELD_KEC]}<br>
+<b>Jumlah:</b> ${getValue(f.properties,jenis.value)}
 `);
 }
 }).addTo(map);
@@ -73,17 +83,17 @@ layer.bindPopup(`
 map.fitBounds(geoLayer.getBounds());
 
 isiKecamatan();
+hitungTotal(geoData.features);
+
 });
 
-/* DROPDOWN KECAMATAN */
+/* ISI KECAMATAN */
 function isiKecamatan(){
-let kecamatan = [...new Set(geoData.features.map(f=>f.properties[FIELD_KEC]))].sort();
-kecamatan.forEach(k=>{
-kecSelect.innerHTML += `<option value="${k}">${k}</option>`;
-});
+let kec=[...new Set(geoData.features.map(f=>f.properties[FIELD_KEC]))].sort();
+kec.forEach(k=>kecSelect.innerHTML+=`<option value="${k}">${k}</option>`);
 }
 
-/* FILTER KECAMATAN */
+/* FILTER KEC */
 kecSelect.onchange=function(){
 
 desaSelect.innerHTML=`<option value="">Semua Desa</option>`;
@@ -91,54 +101,74 @@ geoLayer.resetStyle();
 
 if(!this.value){
 map.fitBounds(geoLayer.getBounds());
+hitungTotal(geoData.features);
 return;
 }
 
-let filtered = geoData.features.filter(f=>f.properties[FIELD_KEC]==this.value);
+let filtered=geoData.features.filter(f=>f.properties[FIELD_KEC]==this.value);
+
 map.fitBounds(L.geoJSON(filtered).getBounds());
+hitungTotal(filtered);
 
-geoLayer.eachLayer(layer=>{
-if(layer.feature.properties[FIELD_KEC]==this.value) highlight(layer);
+geoLayer.eachLayer(l=>{
+if(l.feature.properties[FIELD_KEC]==this.value)highlight(l);
 });
 
-let desaList = [...new Set(filtered.map(f=>f.properties[FIELD_DESA]))].sort();
-desaList.forEach(d=>{
-desaSelect.innerHTML += `<option value="${d}">${d}</option>`;
-});
+let desa=[...new Set(filtered.map(f=>f.properties[FIELD_DESA]))].sort();
+desa.forEach(d=>desaSelect.innerHTML+=`<option value="${d}">${d}</option>`);
+
 };
 
 /* FILTER DESA */
 desaSelect.onchange=function(){
+
 geoLayer.resetStyle();
-geoLayer.eachLayer(layer=>{
-if(layer.feature.properties[FIELD_DESA]==this.value){
-highlight(layer);
-map.fitBounds(layer.getBounds());
+
+let filtered=geoData.features.filter(f=>f.properties[FIELD_DESA]==this.value);
+
+hitungTotal(filtered);
+
+geoLayer.eachLayer(l=>{
+if(l.feature.properties[FIELD_DESA]==this.value){
+highlight(l);
+map.fitBounds(l.getBounds());
 }
 });
+
 };
 
-/* UPDATE TAHUN / JENIS */
+/* UPDATE MAP */
 function updateMap(){
 
-if(!geoLayer) return;
+if(!geoLayer)return;
 
-labelTahun.textContent = tahun.value;
+labelTahun.textContent=tahun.value;
 
 geoLayer.setStyle(style);
 
-geoLayer.eachLayer(layer=>{
-layer.setPopupContent(`
-<b>Desa:</b> ${layer.feature.properties[FIELD_DESA]}<br>
-<b>Kecamatan:</b> ${layer.feature.properties[FIELD_KEC]}<br>
-<b>Jumlah:</b> ${getValue(layer.feature.properties)}
+geoLayer.eachLayer(l=>{
+l.setPopupContent(`
+<b>Desa:</b> ${l.feature.properties[FIELD_DESA]}<br>
+<b>Kecamatan:</b> ${l.feature.properties[FIELD_KEC]}<br>
+<b>Jumlah:</b> ${getValue(l.feature.properties,jenis.value)}
 `);
 });
+
+if(desaSelect.value){
+desaSelect.onchange();
+}
+else if(kecSelect.value){
+kecSelect.onchange();
+}
+else{
+hitungTotal(geoData.features);
+}
+
 }
 
 /* EVENT */
-tahun.addEventListener("input", updateMap);
-jenis.addEventListener("change", updateMap);
+tahun.oninput=updateMap;
+jenis.onchange=updateMap;
 
 prev.onclick=()=>{tahun.stepDown();updateMap();}
 next.onclick=()=>{tahun.stepUp();updateMap();}
